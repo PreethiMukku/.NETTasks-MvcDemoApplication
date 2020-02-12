343b48783d3314f07fd921186a80cd596d208223
Preethi_Mukku <Preethi_Mukku@EPINHYDW015C>
2020-02-12 12:27:48 +0530

    Add project files.

---
+++ MvcDemoApplication/Controllers/StudentDetailsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcDemoApplication.Models;
+
+namespace MvcDemoApplication.Controllers
+{
+    public class StudentDetailsController : Controller
+    {
+        private StudentDatabaseEntities1 db = new StudentDatabaseEntities1();
+
+        // GET: StudentDetails
+        public ActionResult Index()
+        {
+            return View(db.StudentDetails.ToList());
+        }
+
+        // GET: StudentDetails/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetail studentDetail = db.StudentDetails.Find(id);
+            if (studentDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetail);
+        }
+
+        // GET: StudentDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Gender,Age")] StudentDetail studentDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StudentDetails.Add(studentDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(studentDetail);
+        }
+
+        // GET: StudentDetails/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetail studentDetail = db.StudentDetails.Find(id);
+            if (studentDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetail);
+        }
+
+        // POST: StudentDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Gender,Age")] StudentDetail studentDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(studentDetail);
+        }
+
+        // GET: StudentDetails/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetail studentDetail = db.StudentDetails.Find(id);
+            if (studentDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetail);
+        }
+
+        // POST: StudentDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            StudentDetail studentDetail = db.StudentDetails.Find(id);
+            db.StudentDetails.Remove(studentDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
